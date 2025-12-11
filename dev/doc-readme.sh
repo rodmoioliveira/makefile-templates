60317c1c0ff298209abcaa763daf87e2c11b527e
Rodolfo Mói de Oliveira <rodmoi.oliveira@gmail.com>
2025-12-11 08:49:06 -0300

    feat: Move code to sourcehut
    Signed-off-by: Rodolfo Mói de Oliveira <rodmoi.oliveira@gmail.com>

--- dev/doc-readme.sh
+++ dev/doc-readme.sh
@@ -42,6 +42,11 @@ readme() {
 
 A collection of my commonly used makefile recipes.
 
+[![builds.sr.ht status](https://builds.sr.ht/~rodmoi.svg)](https://builds.sr.ht/~rodmoi?)
+
+> [!IMPORTANT]
+>  This repo was [moved](https://git.sr.ht/~rodmoi/neovim) to [sourcehut](https://sr.ht/)!
+
 # index
 
 $(index)
